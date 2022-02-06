87c9698bac1b7f3a701d02d049db27119f09ad0e
j5xiao <81590932+j5xiao@users.noreply.github.com>
2022-02-05 20:37:38 -0800

    work

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process',methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/success')
+
+@app.route('/success')
+def success():
+    return render_template('new.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
